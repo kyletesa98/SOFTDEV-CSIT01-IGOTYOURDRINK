ea4609cd37711b61ee84ce037de515cb926556b4
kyletesa98 <37198187+kyletesa98@users.noreply.github.com>
2018-12-13 19:50:03 +0800

    Uploaded Images

---
+++ database/migrations/2018_12_09_121302_create_drinks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDrinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drinks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('shop_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('shop_id')->references('id')->on('shops');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drinks');
+    }
+}
