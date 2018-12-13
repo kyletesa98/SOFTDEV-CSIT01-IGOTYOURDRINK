4812316adf2007ea388caa3a4606b2e0d0474998
Dan Fernandez <35862656+Suezmeister@users.noreply.github.com>
2018-12-13 20:07:25 +0800

    Eye Candy

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-light bg-light navbar-fixed-top">
     <div class="container-fluid">
-        <a href="/home" class="navbar-brand">I Got Your Drink</a>
+        <a href="/home" class="navbar-brand">Hello, {{ Auth::user()->name }}. I Got Your Drink!</a>
         <a href="/logout" class="btn btn-success my-2 my-sm-0">Log Out</a>
     </div>
 </nav>
